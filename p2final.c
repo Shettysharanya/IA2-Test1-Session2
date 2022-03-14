f285476416480d3f6372cd6e0f03b9631fd17501
sharanya0709 <94777111+Shettysharanya@users.noreply.github.com>
2022-03-14 06:40:31 +0000

    p2final.c

--- p2final.c
+++ p2final.c
@@ -1,31 +1,38 @@
-#include <stdio.h>
-int input_number()
+#include<stdio.h>
+int input_side()
 {
-  int num;
-  printf("Enter the number:\n");
-  scanf("%d", &num);
-  return num;
+  int a;
+  printf("Enter the side of the triangle\n");
+  scanf("%d",&a);
+  return a;
 }
-int is_composite(int n)
+int check_scalene(int a,int b,int c)
 {
-  for (int i = 2; i <= n/2; i++)
+  int count=0;
+  if(a!=b&&b!=c&&c!=a)
   {
-    if (n%i == 0)
-      return 1;
+    count++;
   }
-  return 0;
+  return count;
 }
-void output(int n, int isComposite)
+void output(int a,int b,int c,int isscalene)
 {
-  if (isComposite)
-    printf("%d is a composite number\n", n);
-  else 
-    printf("%d is not a composite number\n", n);
+  if(isscalene==1)
+  {
+    printf("The triangle with the sides %d,%d and %d is Scalene",a,b,c);
+  }
+  else
+  {
+    printf("The triangle with the sides %d,%d and %d is not scalene",a,b,c);
+  }  
 }
 int main()
 {
-  int num = input_number();
-  int res = is_composite(num);
-  output(num, res);
+  int a1,b1,c1,count1;
+  a1=input_side();
+  b1=input_side();
+  c1=input_side();
+  count1=check_scalene(a1,b1,c1);
+  output(a1,b1,c1,count1);
   return 0;
 }
